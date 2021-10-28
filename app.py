367d032ae770ad4998af3e6688de491f7a2ccf69
David Pittman <53283971+dopittman@users.noreply.github.com>
2021-10-27 21:01:04 -0400

    Fix indentation error in CouseDataParser

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from DataParser import CourseDataParser
 
 app = Flask(__name__)
 
+test_url = "http://campusmapuncp.s3-website-us-east-1.amazonaws.com/"
 
 # we are importing our function from the colors.py file
 
 
 @app.route("/", methods=['GET'])
 def index():
-    data = CourseDataParser.get_student_data("/Users/davidpittman/Documents/GitHub/campus-map-web-server/TestPage/TestPage.html")
+    data = CourseDataParser.get_student_data("http://campusmapuncp.s3-website-us-east-1.amazonaws.com/")
     data = flask.jsonify(data)
     return data
